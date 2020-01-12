ea0f4f9df387e914889a84ce6af75b2f3b23b78e
Ali Doğan Aydöner <40503683+adaydoner@users.noreply.github.com>
2020-01-12 22:35:22 +0300

    CustomerDAO interface and its implementation class

---
+++ web-customer-tracker/src/adaydoner/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package adaydoner.springdemo.dao;
+
+import java.util.List;
+
+import adaydoner.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomer();
+
+}
---
+++ web-customer-tracker/src/adaydoner/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,39 @@
+package adaydoner.springdemo.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import adaydoner.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomer() {
+		
+		//get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		//create a query
+		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		
+		//execute query and get result list
+		List<Customer> customers = theQuery.getResultList();
+		
+		//return the results
+		return customers;
+	}
+
+}
