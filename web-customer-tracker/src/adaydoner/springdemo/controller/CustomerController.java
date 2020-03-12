68af6e98503bece651c62546566ddd1f9dc7d4d1
Ali Doğan Aydöner <40503683+adaydoner@users.noreply.github.com>
2020-03-12 21:51:36 +0300

    added delete link and pop-up -- front end

--- web-customer-tracker/src/adaydoner/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/adaydoner/springdemo/controller/CustomerController.java
@@ -17,48 +17,55 @@ import adaydoner.springdemo.service.CustomerService;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	
+
 	@Autowired
 	private CustomerService customerService;
-	
-	
+
 	@GetMapping("/list")
-	public String listCustomers (Model theModel){
-		
+	public String listCustomers(Model theModel) {
+
 		List<Customer> theCustomers = customerService.getCustomers();
-		
-		theModel.addAttribute("customers",theCustomers);
-		
+
+		theModel.addAttribute("customers", theCustomers);
+
 		return "list-customers";
 	}
-	
-	
+
 	@GetMapping("/showFormForAdd")
-	public String showFormForAdd(Model theModel){
-		
+	public String showFormForAdd(Model theModel) {
+
 		Customer theCustomer = new Customer();
-		
-		theModel.addAttribute("customer",theCustomer);
-		
+
+		theModel.addAttribute("customer", theCustomer);
+
 		return "customer-form";
 	}
-	
+
 	@PostMapping("/saveCustomer")
-	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer){
-		
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+
 		customerService.saveCustomer(theCustomer);
-		
+
 		return "redirect:/customer/list";
-		
+
 	}
-	
+
 	@GetMapping("/showFormForUpdate")
-	public String showFormForUpdate(@RequestParam("customerId") int theId, Model theModel){
-		
+	public String showFormForUpdate(@RequestParam("customerId") int theId, Model theModel) {
+
 		Customer theCustomer = customerService.getCustomer(theId);
-		
-		theModel.addAttribute("customer",theCustomer);
-		
+
+		theModel.addAttribute("customer", theCustomer);
+
+		return "customer-form";
+	}
+
+	@GetMapping("/deleteCustomer")
+	public String deleteCustomer(@RequestParam("customerId") int theId, Model theModel) {
+		Customer theCustomer = customerService.getCustomer(theId);
+
+		theModel.addAttribute("customer", theCustomer);
+
 		return "customer-form";
 	}
 }
