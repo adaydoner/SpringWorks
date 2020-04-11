bba2a47c4418a577af666fb9cc97fc2fabf02631
Ali Doğan Aydöner <40503683+adaydoner@users.noreply.github.com>
2020-04-11 12:41:36 +0300

    Create - Read - Update - Delete operations

--- spring-rest-03-API-crm-crud/src/main/java/adaydoner/springdemo/rest/CustomerRestController.java
+++ spring-rest-03-API-crm-crud/src/main/java/adaydoner/springdemo/rest/CustomerRestController.java
@@ -3,8 +3,12 @@ package adaydoner.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,4 +36,58 @@ public class CustomerRestController {
 		}
 		return customer;
 	}
+	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer){
+		// we explicitly settind id 0 because saveOrUpdate method check id and if its empty its insert new entry to the database , else update existing one.
+		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer){
+		customerService.saveCustomer(theCustomer);
+		return theCustomer;
+	}
+	
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId){
+		Customer tmpCustomer = customerService.getCustomer(customerId);
+		
+		if(tmpCustomer == null){
+			throw new CustomerNotFoundException("customer id not found - " + customerId);
+		}
+		
+		customerService.deleteCustomer(customerId);
+		
+		return ("Deleted customer id -"+ customerId); 
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
