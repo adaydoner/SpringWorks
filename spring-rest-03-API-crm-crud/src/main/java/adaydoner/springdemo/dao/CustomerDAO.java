68d42e81f93d2c8f37613c888e579daa63b7d661
Ali Doğan Aydöner <40503683+adaydoner@users.noreply.github.com>
2020-04-11 03:09:03 +0300

    CRM API

---
+++ spring-rest-03-API-crm-crud/src/main/java/adaydoner/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package adaydoner.springdemo.dao;
+
+import java.util.List;
+
+import adaydoner.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+
+}
---
+++ spring-rest-03-API-crm-crud/src/main/java/adaydoner/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package adaydoner.springdemo.service;
+
+import java.util.List;
+
+import adaydoner.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
